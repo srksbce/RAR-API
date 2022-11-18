4bfebb3c0529db5b98b53e6bb66ab894cbdff02e
srksbce <srk.sbce@gmail.com>
2022-11-18 15:10:34 +0530

    commit

---
+++ Angular/Angular.Repository/Interfaces/IRegsiterRepository.cs
@@ -0,0 +1,16 @@
+﻿using Angular.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angular.Repository.Interfaces
+{
+    public interface IRegsiterRepository
+    {
+        Task<List<RegisterUser>> GetRegisters();
+        Task<RegisterUser> InsertRegister(RegisterUser reg);
+        Task<Reg> InRegister(Reg reg);
+    }
+}
